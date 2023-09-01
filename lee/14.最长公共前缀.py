e346631cc0c55f17a892c51bb51c069d58755294
wangjize <y98d225@163.com>
2023-09-01 21:48:49 +0800

    首次编写最长公共前缀报错：原因用利有可能不走st，这样使得return的st无值

--- lee/14.最长公共前缀.py
+++ lee/14.最长公共前缀.py
@@ -7,5 +7,11 @@
 # @lc code=start
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs)):
+            s = [char[:i+1] for char in strs]
+            if len(set(s)) == 1:
+                st = s[0]
+            else:
+                break
+        return st
 # @lc code=end
-
