df09615893af2ac1d3b129b5b61980e600dc9e2e
wangjize <y98d225@163.com>
2023-09-01 21:53:41 +0800

    【最长公共前缀】修改首次编写错误

--- lee/14.最长公共前缀.py
+++ lee/14.最长公共前缀.py
@@ -7,6 +7,10 @@
 # @lc code=start
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        # 首次编写，本次代码执行用例报错。
+        # 原因：用例中可能没有公共前缀，这样就不会走if分支。st也就未定义。在返回时提示st就会出现异常。
+        # 修改方法：先初始化st字符串
+        st = ""
         for i in range(len(strs)):
             s = [char[:i+1] for char in strs]
             if len(set(s)) == 1:
