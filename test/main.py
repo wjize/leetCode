84a102c7d7de24e9035f1b5ed60348af7194ceac
wangjize <y98d225@163.com>
2023-09-02 09:51:15 +0800

    test

--- test/main.py
+++ test/main.py
@@ -2,19 +2,30 @@ while True:
     ent = input("是否继续(y/n):")
     if ent == "n":
         break
-    else:
-        input_string = input("请输入一个用逗号分隔的列表:")
-        strs = input_string.split(',')
+    input_string = input("请输入:")
+    # strs = input_string.split(',')
 
-        st = ""
-        min_len = min(len(s) for s in strs)
-        # for i in range(len(strs)):
-        for i in range(min_len):
-            char = strs[0][i]
-            # s = [char[:i+1] for char in strs]
-            #if len(set(s)) == 1:
-            if all(s[i] == char for s in strs):
-                st += char
-            else:
-                break
-        print(st)
+    class Solution:
+        def isValid(self, s: str) -> bool:
+            base_bracket = {")":"(", "]":"[", "}":"{"}
+            stack = []
+            if len(s) == 1 or s[0] in [")", "]", "}"]:
+                return False
+            for i in range(len(s)):
+                if s[i] in ["(", "[", "{"]:
+                    stack.append(s[i])
+                if s[i] in [")", "]", "}"]:
+                    if len(stack) == 0 or stack.pop() != base_bracket[s[i]]:
+                        # bo = True
+                    # else:
+                        return False
+            # return bo
+            # return True
+            # if len(stack) == 0:
+            #     return True
+            # else:
+            #     return False
+            return len(stack) == 0
+
+    mytest = Solution()
+    print(mytest.isValid(input_string))
