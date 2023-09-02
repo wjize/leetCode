0992e66746572cb88256af2817f6d4e9123715ad
wangjize <y98d225@163.com>
2023-09-02 11:42:26 +0800

    Feature #13-1 【罗马数字转整数】简化代码

--- lee/13.罗马数字转整数.py
+++ lee/13.罗马数字转整数.py
@@ -28,6 +28,8 @@ class Solution:
         ' 优化: 将特殊字符转换成正常算数逻辑字符
         ' 如: 若s = MIV 中有IV, 则先将其替换为IIII, 此时s变化为MIIII。这样就可以用正常逻辑计算值了
         '''
+
+        # Feature #13-1 【罗马数字转整数】简化代码
         sum = 0
         base_str = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         s = s.replace("IV","IIII").replace("IX","VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
