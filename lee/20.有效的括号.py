de19bdab24b63c643523ec6ed9aba3df82bb4781
wangjize <y98d225@163.com>
2023-09-02 08:34:28 +0800

    Bug #20-1 【有效的括号】当用例为一个字符并且为左括号时，会导致bo无值，return时报错

--- lee/20.有效的括号.py
+++ lee/20.有效的括号.py
@@ -7,6 +7,9 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
+
+        # Bug #20-1 【有效的括号】当用例为一个字符并且为左括号时，会导致bo无值，return时报错
+        # 修改方法，if判断语句修改为 != 这样可减少一次判断，并不会出现问题
         base_bracket = {")":"(", "]":"[", "}":"{"}
         stack = []
         for str in s:
@@ -14,10 +17,8 @@ class Solution:
                 stack.append(str)
             if str == ")" or str == "]" or str == "}":
                 if stack.pop() == base_bracket[str]:
-                    bo = True
-                else:
                     return False
-        return bo
+        return True
 
 # @lc code=end
 
