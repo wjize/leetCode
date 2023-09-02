62c9d1f359713447aedf28d4c3457f4e5a39538f
wangjize <y98d225@163.com>
2023-09-02 11:25:18 +0800

    Feature #21-1 【合并两个有序链表】简化代码

--- lee/21.合并两个有序链表.py
+++ lee/21.合并两个有序链表.py
@@ -25,20 +25,26 @@ class Solution:
         # 原因：缺少一个步骤，当两个列表其中一个遍历结束后，应该将另一个链表直接加进去。不然就会出现上述最后缺数据的情况。
         # 修改方法：循环前的条件判断放到循环后执行。
 
+        # Feature #21-1 【合并两个有序链表】简化代码
+        # 优化：逻辑方面无需太多优化，只需将代码尽可能缩短，提高代码可读性
+
         while list1 and list2:
             if list1.val <= list2.val:
-                current.next = list1
-                list1 = list1.next
+                # 注意：该写法一定注意等号后的第一个元素，一定是赋值等号前的第一个元素。顺序不可乱
+                current.next, list1 = list1, list1.next
+                # current.next = list1
+                # list1 = list1.next
             else:
-                current.next = list2
-                list2 = list2.next        
+                current.next, list2 = list2, list2.next
+                # current.next = list2
+                # list2 = list2.next        
             current = current.next
 
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-
+        # if list1:
+        #     current.next = list1
+        # elif list2:
+        #     current.next = list2
+        current.next = list1 or list2
         return head.next
 
 # @lc code=end
